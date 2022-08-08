6b34e331e1b08023c716893c201c54ad9382615d
Flávio Bianchetti <fjb.developer@gmail.com>
2022-08-08 20:30:34 -0300

    fix:(word_search.py) Função exists_word - complexidade
    Detalhes:
    - Criada função create_output_words_found para diminuir complexidade da
      função exists_word.
    - Removida chamada 'import sys'
    
    Requisito 6 corrigido e atendido.

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,22 @@
-import sys
 from ting_file_management.file_management import txt_importer
+
+
+def create_output_words_found(path_file, word):
+    file = txt_importer(path_file)
+    if file is not None:
+        foundWordList = []
+        for index, line in enumerate(file):
+            if word.lower() in line.lower():
+                foundWordList.append({
+                    'linha': index + 1
+                })
+        if len(foundWordList):
+            return {
+                'palavra': word,
+                'arquivo': path_file,
+                'ocorrencias': foundWordList,
+            }
+    return None
 
 
 def exists_word(word, instance):
@@ -8,21 +25,10 @@ def exists_word(word, instance):
     for index in range(0, len(instance)):
         inst.append(instance.search(index))
 
-    for item in inst:
-        file = txt_importer(item)
-        if file is not None:
-            foundWordList = []
-            for index, line in enumerate(file):
-                if word.lower() in line.lower():
-                    foundWordList.append({
-                        'linha': index + 1
-                    })
-            if len(foundWordList):
-                result.append({
-                    'palavra': word,
-                    'arquivo': item,
-                    'ocorrencias': foundWordList,
-                })
+    for path_file in inst:
+        output_file = create_output_words_found(path_file, word)
+        if output_file is not None:
+            result.append(output_file)
     return result
 
 
