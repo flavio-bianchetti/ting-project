fad7de52cae775190f84338612a30f7724986811
Flávio Bianchetti <fjb.developer@gmail.com>
2022-08-08 17:05:20 -0300

    feat:(file_process.py) Função process para processamento do conteúdo
    Detalhes:
    - Implementada função process dentro do módulo file_process capaz
      de ler o arquivo carregado na função anterior e efetuar o pré-
      processamento do conteúdo.
    
    Requisito 3 atendido.

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,23 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
+# https://www.geeksforgeeks.org/how-to-print-to-stderr-and-stdout-in-python/
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    inst = list()
+    for index in range(0, len(instance)):
+        inst.append(instance.search(index))
+
+    if path_file not in inst:
+        instance.enqueue(path_file)
+        file = txt_importer(path_file)
+        if file is not None:
+            result = {
+                'nome_do_arquivo': path_file,
+                'qtd_linhas': len(file),
+                'linhas_do_arquivo': file,
+            }
+            print(f'{result}', file=sys.stdout)
 
 
 def remove(instance):
