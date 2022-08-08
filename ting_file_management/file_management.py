d1db06726b1e7750a8d0f84b7dab199746895282
Flávio Bianchetti <fjb.developer@gmail.com>
2022-08-08 16:10:34 -0300

    feat:(file_management.py) Função txt_importer para importação de notícias
    Detalhes:
    - Implementada função txt_importer dentro do módulo file_management
      capaz de importar notícias a partir de um arquivo TXT, utilizando
      "\n" como separador.
    
    Requisito 2 atendido.

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,21 @@
+import sys
+
+
+# https://pt.stackoverflow.com/questions/495999/validar-upload-de-arquivos-csv
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    extensions = ['txt']
+    lines = list()
+    try:
+        with open(path_file, mode='r') as file:
+            ext = file.name.lower().split('.')[-1]
+            if ext not in extensions:
+                print("Formato inválido", file=sys.stderr)
+
+            for line in file.readlines():
+                lines.append(line.split('\n')[0])
+
+        return lines
+
+    # https://docs.python.org/3/library/exceptions.html
+    except OSError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
